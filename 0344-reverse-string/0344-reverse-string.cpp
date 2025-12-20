ee1b543381752427f946c98b9da37d2af7f2f3d5
Divya Sinha <119342256+Divyasinha111@users.noreply.github.com>
2025-12-20 12:56:26 +0530

    Time: 0 ms (100%), Space: 27.1 MB (97.31%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int start=0;
+        int n =s.size();
+        int end=n-1;
+        
+      
+        while(start<end){
+            swap (s[start],s[end]);
+            start++;
+            end--;
+        }
+       
+    }
+};
